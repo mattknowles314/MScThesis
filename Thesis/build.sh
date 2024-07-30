e589a4ea69c361e430df67a6903e47bec7c3dda7
Matthew Knowles <mattknowles314@gmail.com>
2024-07-30 15:32:05 +0100

    remove the PCNMA docs flag and improve compilation time

--- Thesis/build.sh
+++ Thesis/build.sh
@@ -1,21 +1,12 @@
-#!/bin/bash
+#/bin/bash
 
-RUN_R_COMMAND=false
-
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -r|--run-r) RUN_R_COMMAND=true; shift ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-done
-
-if [ "$RUN_R_COMMAND" = true ]; then
-    R CMD Rd2pdf ../../PCMNMA
-    mv PCNMA.pdf Chapters/
-fi
-
-xelatex main.tex
+start=$(date +%s)
+xelatex -no-pdf main.tex 
 bibtex main
-xelatex main.tex
+xelatex -no-pdf main.tex
 xelatex main.tex
 texcount -inc -html -v -sum main.tex > wordcount.html
+end=$(date +%s)
+runtime=$((end-start))
+echo "compiletime: $runtime s"
+
